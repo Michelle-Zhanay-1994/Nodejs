a79a2c951b50e9319847a6ac05953d50bcd665be
Michelle <michu18deabril1994@gmail.com>
2020-09-04 15:08:07 -0500

    El primer trabajo pagina donde pone Hola mundo

---
+++ Index.js
@@ -0,0 +1,13 @@
+const express=requiere("express")
+const bodyparser=requiere('body-parse')
+var http=require("http");
+var server=http.createServer();
+function mensaje(peticion,respuesta){
+    respuesta.writeHead(200,{'content-type':'text/plain'})
+    respuesta.write("Hola Mundo")
+    respuesta.end();
+}
+server.on('request',mensaje);
+server.listen(3000,function(){
+    console.log("El servidor se inicio correctamente")
+});
